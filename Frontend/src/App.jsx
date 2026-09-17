577ee9293850c0e84b76babdf6c3bb3f9f6d114a
Parth Gorasiya <parthgorasiya77@gmail.com>
2026-09-17 16:32:25 -0500

    Frontend Setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Sidebar from './Sidebar';
+import ChatWindow from './ChatWindow';
+import {MyContext} from './MyContext.jsx';
+
+function App() {
+  const providerValues = {};
+
+  return (
+   <div className="app">
+    <MyContext.Provider value = {providerValues}>
+    <Sidebar />
+    <ChatWindow />
+    </MyContext.Provider>
+   </div>
+  )
+}
+
+export default App
