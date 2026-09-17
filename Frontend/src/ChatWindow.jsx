f6ff6c3376ccdaa38d2ab6d0063d3e4e61cdab08
Parth Gorasiya <parthgorasiya77@gmail.com>
2026-09-17 17:46:38 -0500

    Build and Styled ChatWindow

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,8 +1,32 @@
-import "./ChatWindow.css"
+import "./ChatWindow.css";
+import Chat from './Chat.jsx';
 
 function ChatWindow() {
   return (
-    <div>ChatWindow</div>
+    <div className="chatWindow">
+      <div className="navbar">
+        <span>SigmaGPT <i className="fa-solid fa-chevron-down"></i></span>
+        <div className="userIconDiv" >
+          <span className="userIcon"><i className="fa-solid fa-user"></i></span>
+        </div>
+
+      </div>
+      <Chat> </Chat>
+
+      <div className="chatInput">
+        <div className="inputBox">
+          <input placeholder="Ask anything" >
+
+          </input>
+          <div id="submit"> <i className="fa-solid fa-paper-plane"></i></div>
+        </div>
+        <p className="info">
+          SigmaGPT can make mistakes. Check important info. See Cookie Preferences.
+        </p>
+      </div>
+
+    </div>
+
   )
 }
 
