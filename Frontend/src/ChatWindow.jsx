3d5bed72bd63338d4c3d84f6c0b0948adcd65889
Parth Gorasiya <parthgorasiya77@gmail.com>
2026-09-17 18:51:34 -0500

    Added Get Reply for Prompt Functionality

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -1,7 +1,38 @@
 import "./ChatWindow.css";
 import Chat from './Chat.jsx';
+import { MyContext } from "./MyContext.jsx";
+import { useContext, useState } from "react";
+import {ScaleLoader} from 'react-spinners';
 
 function ChatWindow() {
+
+  const {prompt, setPrompt, reply, setReply, currThreadId} = useContext(MyContext);
+  const [loading, setLoading] = useState(false);
+  const getReply = async () => {
+    setLoading(true);
+    const options = {
+      method : "POST",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body : JSON.stringify({
+        message: prompt,
+        threadId : currThreadId
+      })
+    };
+
+    try {
+      const response = await fetch("http://localhost:8080/api/chat", options);
+      const res = await response.json(); //reply from assistant
+      console.log(res);
+      setReply(res.reply);
+      
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  }
+
   return (
     <div className="chatWindow">
       <div className="navbar">
@@ -12,13 +43,17 @@ function ChatWindow() {
 
       </div>
       <Chat> </Chat>
+      <ScaleLoader color= '#fff' loading = {loading} />
 
       <div className="chatInput">
         <div className="inputBox">
-          <input placeholder="Ask anything" >
-
-          </input>
-          <div id="submit"> <i className="fa-solid fa-paper-plane"></i></div>
+          <input placeholder="Ask anything" 
+          value = {prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+          onKeyDown={(e) => e.key === 'Enter' ? getReply() : ''} 
+          />
+  
+          <div id="submit" onClick={getReply}> <i className="fa-solid fa-paper-plane"></i></div>
         </div>
         <p className="info">
           SigmaGPT can make mistakes. Check important info. See Cookie Preferences.
