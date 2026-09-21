11c58d41ad5edf647d4571ff0e7ac19f7dc40ad7
Parth Gorasiya <parthgorasiya77@gmail.com>
2026-09-21 16:43:38 -0500

    Add typing effect and rich Markdown rendering

--- Frontend/src/Chat.jsx
+++ Frontend/src/Chat.jsx
@@ -1,24 +1,62 @@
 import "./Chat.css";
-import { useContext } from "react";
+import { useContext, useState , useEffect} from "react";
 import { MyContext } from "./MyContext";
+import ReactMarkdown from "react-markdown";
+import rehypeHighlight from 'rehype-highlight';
+import "highlight.js/styles/github-dark.css";
+import remarkGfm from "remark-gfm";
 
 function Chat() {
-   const {newChat, prevChats} = useContext(MyContext);
+   const {newChat, prevChats, reply} = useContext(MyContext);
+   const [latestReply, setLatestReply] = useState(null);
+
+   useEffect(() => {
+  if (!prevChats?.length || !reply) {
+    return;
+  }
+
+  // Preserve spaces and newline characters
+  const content = reply.split(/(\s+)/);
+
+  let idx = 0;
+  setLatestReply("");
+
+  const interval = setInterval(() => {
+    setLatestReply(content.slice(0, idx + 1).join(""));
+
+    idx++;
+
+    if (idx >= content.length) {
+      clearInterval(interval);
+    }
+  }, 40);
+
+  return () => clearInterval(interval);
+}, [prevChats, reply]);
 
   return (
    <>
    {newChat && <h1>Start a New Chat!</h1>}
    <div className="chats">
     {
-      prevChats ?.map((chat, idx) => 
+      prevChats?.slice(0,-1).map((chat, idx) => 
       <div className= {chat.role === "user" ? "userDiv" : "gptDiv"} key = {idx}>
         {
           chat.role === 'user' ?
           <p className="userMessage">{chat.content}</p> :
-          <p className="gptMessage">{chat.content}</p> 
+          <ReactMarkdown
+  remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight]}>{chat.content}</ReactMarkdown>
         }
       </div>
     )
+    }
+
+    {
+      prevChats?.length > 0 && latestReply !== null &&
+
+      <div className="gptDiv" key={"typing"}>
+        <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeHighlight]}>{latestReply}</ReactMarkdown> 
+        </div>
     }
   
     
