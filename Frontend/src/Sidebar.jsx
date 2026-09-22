73a1e8856bf2a03727b42bb2e1330621e6dbb774
Parth Gorasiya <parthgorasiya77@gmail.com>
2026-09-22 14:40:09 -0500

    Add thread deletion and sidebar chat management

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -48,11 +48,28 @@ function Sidebar() {
         }
     }   
 
+    const deleteThread = async (threadId) => {
+      try {
+        const response = await fetch (`http://localhost:8080/api/thread/${threadId}`, {method: "DELETE"});
+        const res = await response.json();
+        console.log(res);
+
+        //updated threads re-render
+        setAllThreads(prev => prev.filter(thread => thread.threadId !== threadId));
+
+        if(threadId === currThreadId){
+          createNewChat();
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
 
   return (
     <section className="sidebar">
       <button onClick={createNewChat}>
-        <img src='src/assests/blacklogo.png' alt='gpt logo' className="logo"></img>
+        <img src='src/assets/blacklogo.png' alt='gpt logo' className="logo"></img>
        <span><i className="fa-solid fa-pen-to-square"></i></span> 
       </button>
 
@@ -61,7 +78,14 @@ function Sidebar() {
           allThreads?.map((thread, idx) => (
             <li key={idx} onClick={(e)  => changeThread(thread.threadId)}
               className={thread.threadId === currThreadId ? "highlighted": " "}
-              >{thread.title}</li>
+              >{thread.title}
+              <i className="fa-solid fa-trash"
+              onClick={(e) => {
+                e.stopPropagation(); // stop event bubbling
+                deleteThread(thread.threadId);
+              }}
+              ></i>
+              </li>
           ))
         }
       </ul>
